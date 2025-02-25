fa4f5fbb8c955458148e65f4cac09c36ca24e337
Daniel Zima <128959089+ZimaDaniel@users.noreply.github.com>
2025-02-25 18:47:53 +0100

    Update volume-up.sh

--- volume-up/.local/bin/volume-up.sh
+++ volume-up/.local/bin/volume-up.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+# Získá aktuální hlasitost prvního výchozího sinku
+current_vol=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+(?=%)' | head -1)
+# Pokud je hlasitost menší než 100, zvýší ji o 10%
+if [ "$current_vol" -lt 100 ]; then
+    pactl set-sink-volume @DEFAULT_SINK@ +10%
+else
+    # Pokud už je 100 nebo více, nastaví ji pevně na 100%
+    pactl set-sink-volume @DEFAULT_SINK@ 100%
+fi
